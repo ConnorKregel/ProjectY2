ceae4607f4dcf0e19b6123c36464eb2a5f9861d7
CameronDelaney2711 <x00207491@mytudublin.ie>
2025-03-03 13:13:19 +0000

    home page working

--- off_License_Store/urls.py
+++ off_License_Store/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('', include('shop.urls')),
+    path('search/', include('search_app.urls')),
 ]#+ static(settings.MEDIA_URL, document_root=setting.MEDIA_ROOT)
