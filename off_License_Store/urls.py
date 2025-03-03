3a4a640326d93ce7d14554e827eb1975aaded46b
CameronDelaney2711 <x00207491@mytudublin.ie>
2025-03-03 14:40:35 +0000

    formatting on cart works i think?

--- off_License_Store/urls.py
+++ off_License_Store/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('shop.urls')),
     path('search/', include('search_app.urls')),
-    path('cart/', include('cart.urls', namespace='cart')),
+    path('cart/', include('cart.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
