8006aa9cef2b26a1034ab31f83c225b718755e1f
Cameron <X00207491@mytudublin.ie>
2025-04-21 23:17:39 +0100

    contact page added to the navbar

--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ContactView, SuccessView
 
+app_name='contact'
+
 urlpatterns = [
     path('contact/', ContactView.as_view(), name='contact'),
     path('success/', SuccessView.as_view(), name='success'),
